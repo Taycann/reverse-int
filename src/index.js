5ba87ee73e59852f4d6debba25b1053d3442891e
Taycann <non.andrew@yandex.ru>
2022-08-12 01:54:40 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if((String(n))[0] === '-') {
+        let result = String(Math.abs(n)).split('').reverse().join('');
+        return Number(result);
+      } else if((String(n))[0] !== '-') {
+        let result = String(n).split('').reverse().join('');
+        return Number(result);
+      }
 }
+
